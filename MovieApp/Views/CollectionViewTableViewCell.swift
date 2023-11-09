864cc4a99adc7cc23427fbd23cd738726f3417f6
Saba <sgogr2021@agruni.edu,ge>
2023-11-10 03:13:21 +0400

    feat: setup homeVC TableView

---
+++ MovieApp/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  MovieApp
+//
+//  Created by Saba Gogrichiani on 10.11.23.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
